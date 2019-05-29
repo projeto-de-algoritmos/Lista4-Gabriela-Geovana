192f6de1b85b9157dd8ab3842b563beec8ae4eed
GeovanaRamos <geovannna.1998@gmail.com>
2019-05-29 09:59:47 -0300

    Added map and element classes

---
+++ src/Element.java
@@ -0,0 +1,39 @@
+
+public class Element {
+	public static final int PERSON = 0;
+	public static final int CAR = 1;
+	private int x;
+	private int y;
+	private int type;
+	
+	public Element(int x, int y, int type) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+}
---
+++ src/Map.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Map {
+	private ArrayList<Element> elements;
+	
+	
+	//public ArrayList<Element> closestPair(ArrayList<Element> elements){
+		// Elements must be sorted according to x-coordinate.
+		
+		// Compute separation line L such that half the points 
+		// are on one side and half on the other side.
+		
+		//c1 = Closest-Pair(left half)
+		//c2 = Closest-Pair(right half)
+		//c3 = min( c1 , c2 )
+		
+		// Delete all points further than from separation line L
+		
+		// Sort remaining points by y-coordinate.
+		
+		//Scan points in y-order and compare distance between
+		//each point and next 6 neighbors. If any of these
+		//distances is less than c3 , update c3.
+		
+		//return c3;
+	//}
+}
